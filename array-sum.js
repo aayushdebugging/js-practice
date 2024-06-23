11bc26494a14e74673ca2f9fca9ad52895d91dd9
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-06-23 22:18:05 +0530

    practice

---
+++ array-sum.js
@@ -0,0 +1,8 @@
+//sum of array
+let arr = [1,3,4,5,6,7,9];
+
+let sum=0;
+for(let i = 0;i<arr.length;i++){
+    sum+=arr[1];
+}
+ console.log(sum);
---
+++ objects.js
@@ -0,0 +1,15 @@
+//create a object representing a car with properties like make , model and year . 
+//Add a method to the car object to start the engine
+
+let car = {
+    make:"A8",
+    model :"Audi",
+    year:2017
+}
+
+function startEngine(){
+    console.log("Enginr Started")
+}
+
+console.log(car);
+console.log(startEngine);
