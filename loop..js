963ec29d53fbc591720f57d5deda89f1d0cd771c
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-06-23 19:59:41 +0530

    practice

---
+++ loop..js
@@ -0,0 +1,17 @@
+//loop
+for (let i = 1 ; i<=5;i++){
+    console.log(i);
+}
+
+//while loop
+let i = 1;
+while(i<=5){
+    console.log(i);
+}
+
+//do-while loop
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i<=5);
