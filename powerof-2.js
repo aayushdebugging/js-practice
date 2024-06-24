a958ac4874232d59aa412f6d5a1aaa0e0b2670fd
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-06-24 12:44:46 +0530

    practice

--- powerof-2.js
+++ powerof-2.js
@@ -1,3 +1,4 @@
+//normal code
 function isPowerOfTwo(n){
     if(n<1){
       return false
@@ -11,3 +12,14 @@ function isPowerOfTwo(n){
     return true
   }
    let ans = console.log(isPowerOfTwo(8))
+
+//use of bitwise operator 
+// This function checks if a number is a power of two by verifying if exactly one bit is set in its binary representation. 
+// If n is a power of two, the binary representation will have a single '1' bit (e.g., 1 = 0b1, 2 = 0b10, 4 = 0b100). 
+// Performing (n & (n - 1)) will result in 0 only for powers of two, because (n - 1) flips all bits after the single '1' bit in n.
+function isPowerOfTwoBitwise(n){
+    if(n<1){
+        return false
+    }
+    return(n & (n-1))===0
+}
