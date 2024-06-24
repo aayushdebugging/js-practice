b90af0c1cbdb967f4e40164f0bf09de82b8f385c
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-06-24 17:06:26 +0530

    practice

--- loop..js
+++ loop..js
@@ -1,17 +1,20 @@
-//loop
-for (let i = 1 ; i<=5;i++){
+console.log("Using for loop:");
+for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 
-//while loop
+// Using while loop
+console.log("Using while loop:");
 let i = 1;
-while(i<=5){
+while (i <= 5) {
     console.log(i);
+    i++; // Incrementing i inside the loop
 }
 
-//do-while loop
-let i = 1;
-do{
+// Using do-while loop
+console.log("Using do-while loop:");
+i = 1; // Resetting i to 1
+do {
     console.log(i);
-    i++;
-}while(i<=5);
+    i++; // Incrementing i inside the loop
+} while (i <= 5);
