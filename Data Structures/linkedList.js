368149a0fbb51b46819839ca2b70b7f5c7573a8e
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-07-22 12:56:25 +0530

    reverse linked list

--- Data Structures/linkedList.js
+++ Data Structures/linkedList.js
@@ -119,6 +119,42 @@ class LinkedList{
         }
     }
 
+//Search method in a Linked List
+    search(value){
+        if(this.isEmpty()){
+            return -1
+        }
+        let i = 0
+        let curr = this.head
+        while(curr){
+            if(curr.value === value){
+                return i
+            }
+            curr = curr.next
+            i++
+        }
+        return -1 
+    }
+
+
+
+    //reverse linkedList
+    reverse(){
+        let prev = null
+        let curr = this.head
+        while(curr){
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        this.head = prev
+
+    }
+
+
+
+
     print(){
         if(this.isEmpty()){
             console.log('List is Empty')
